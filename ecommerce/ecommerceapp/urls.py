0d22e310de9b43d3bd5ab8964ad02f7d8a54c12e
Brian Nkhata <bnkhata@mra.mw>
2024-06-29 19:35:40 +0200

    setting up front end

--- ecommerce/ecommerceapp/urls.py
+++ ecommerce/ecommerceapp/urls.py
@@ -4,4 +4,6 @@ from ecommerceapp import views
 
 urlpatterns = [
     path('', views.index, name="index"),
+    path('contact', views.contact, name="contact"),
+    path('about', views.about, name="about"),
 ]
