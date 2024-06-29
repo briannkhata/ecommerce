ceba65c20769dbf40a7d67c396d7e94ebeaff972
Brian Nkhata <bnkhata@mra.mw>
2024-06-29 21:03:51 +0200

    chnaged frontend template

--- ecommerce/ecommerceapp/urls.py
+++ ecommerce/ecommerceapp/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.index, name="index"),
     path('contact', views.contact, name="contact"),
     path('about', views.about, name="about"),
+    path('signin', views.signin, name="signin"),
 ]
--- ecommerce/ecommerceapp/views.py
+++ ecommerce/ecommerceapp/views.py
@@ -13,3 +13,7 @@ def contact(request):
 
 def about(request):
     return render(request, "about.html")
+
+
+def signin(request):
+    return render(request, "signin.html")
