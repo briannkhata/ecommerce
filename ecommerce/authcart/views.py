c65cdf4dc706693186fb43d0e4dbe4e6aafe3d11
Brian Nkhata <bnkhata@mra.mw>
2024-06-30 12:04:52 +0200

    refactoring login and signup designs

--- ecommerce/authcart/views.py
+++ ecommerce/authcart/views.py
@@ -1,9 +1,29 @@
-from django.shortcuts import render,redirect
-
+from django.shortcuts import render, redirect, HttpResponse
+from django.contrib.auth.models import User
 # Create your views here.
 
 
 def signup(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['pass1']
+        confirm_password = request.POST['pass2']
+
+        if (password != confirm_password):
+            return HttpResponse("Password do not match")
+            # return render(request,'auth/signup.html')
+
+        try:
+            if User.objects.get(username=email):
+                return HttpResponse("Email already exists")
+                # return render(request, 'auth/signup.html')
+        except Exception as e:
+            pass
+        user = User.objects.create_user(email, email, password)
+        user.save()
+        return HttpResponse("User Created", email)
+    else:
+        print("get")
     return render(request, 'authentication/signup.html')
 
 
